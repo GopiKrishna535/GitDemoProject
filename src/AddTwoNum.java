df20870740636c333710bef9f38189468e9186fd
Gopikrishna535 <gopigundapuneni4@gmail.com>
2023-05-18 16:17:38 -0500

    First Commit......

---
+++ src/AddTwoNum.java
@@ -0,0 +1,21 @@
+
+public class AddTwoNum 
+{
+	public  int sum(int n1,int n2)
+	{
+		int sum=n1+n2;
+		return sum;
+	}
+
+	public static void main(String[] args) 
+	{
+		AddTwoNum sot=new AddTwoNum();
+		int add=sot.sum(2,3);
+		
+		System.out.println("Sum of Two numbers="+add);
+	}	
+		
+
+	
+
+}
